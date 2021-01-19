a027861f305534276ab3402ec8d08617f4abedfa
Anirudh Banerjee <imanirudh.dev@gmail.com>
2021-01-19 23:23:28 +0530

    Designer Pdf Viewer

---
+++ designer_pdf_viewer.js
@@ -0,0 +1,53 @@
+function designerPdfViewer(h, word) {
+  const letter = genCharArray('a', 'z')
+  const obj = {}
+  var max = Number.MIN_SAFE_INTEGER
+  for (let i = 0; i < h.length; i++) {
+    obj[letter[i]] = h[i]
+  }
+  for (let i of word) {
+    if (obj[i] > max) {
+      max = obj[i]
+    }
+  }
+  return max * word.length
+}
+function genCharArray(charA, charZ) {
+  var a = []
+  var charA = charA.charCodeAt(0)
+  var charZ = charZ.charCodeAt(0)
+  for (let i = charA; i <= charZ; i++) {
+    a.push(String.fromCharCode(i))
+  }
+  return a
+}
+const letterHeight = [
+  1,
+  3,
+  1,
+  3,
+  1,
+  4,
+  1,
+  3,
+  2,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  7,
+]
+const word = 'abc'
+console.log(designerPdfViewer(letterHeight, word))
