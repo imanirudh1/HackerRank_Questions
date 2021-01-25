a9728fcc77925c8d6290225f51b5e79323eb12ab
Anirudh Banerjee <imanirudh.dev@gmail.com>
2021-01-25 22:16:51 +0530

    Sherlock and Squares Added another method

--- sherlock_and_squares.js
+++ sherlock_and_squares.js
@@ -2,5 +2,10 @@ function squares(a, b) {
   let start = Math.ceil(Math.sqrt(a))
   let end = Math.floor(Math.sqrt(b))
   return end - start + 1
+  //   var count = 0
+  //   for (let i = a; i <= b; i++) {
+  //     if (Math.sqrt(i) % 1 == 0) count++
+  //   }
+  //   return count
 }
-console.log(squares(3, 9))
+console.log(squares(25, 49))
